24fc13510bcf6c5a247ca3afd1b393de1ba5c2e4
Kendall Reid <kreid415@gmail.com>
2024-11-20 11:42:07 -0500

    update based on ARCH feedback

--- launch_nf.sh
+++ launch_nf.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-#SBATCH --partition express
-#SBATCH --mem 5G
-#SBATCH -c 1
-#SBATCH -t 1:00:00
+#SBATCH --job-name=Nextflow_test
+#SBATCH --time=04:00:00
+#SBATCH --mail-user=kreid20@jhu.edu
+#SBATCH --partition=shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+
+
+module load java/19
+
 
 WORKFLOW=$1
 CONFIG=$2
-
-module load nextflow
 
 nextflow -C ${CONFIG} run ${WORKFLOW}
